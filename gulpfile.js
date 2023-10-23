f81f5612e5baacca015317c1486625d10dd60e0f
Arsen <arsen.madzaev@inbox.ru>
2023-10-23 17:53:19 +0300

    restore

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,17 @@ const { src, dest, watch, parallel, series } = require('gulp');
 // const sccs = require('gulp-sass')(require('sass'));
 const sass = require('gulp-dart-sass');
 const sccs = require('gulp-dart-sass');
-const concat = require('gulp-concat')
+const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
+const cheerio = require('gulp-cheerio'); // Удаление лишних атрибутов из SVG
+const svgSprite = require('gulp-svg-sprite');
+const touch = require('gulp-touch-fd');
+const replace = require('gulp-replace');
 
 function browsersync() {
     browserSync.init({
@@ -27,9 +32,12 @@ function scripts() {
         // 'node_modules/jquery/dist/jquery.js',
         'app/js/main.js'
     ])
+    .pipe(sourcemaps.init())
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(sourcemaps.write('./'))
     .pipe(browserSync.stream())
 }
 
@@ -61,6 +69,32 @@ function styles() {
         .pipe(browserSync.stream())
 }
 
+// function svgSprites() {
+//    return src('app/svg/svg-symbols.svg')
+//         .pipe(cheerio({
+//             run: function ($) {
+//                 $('[fill]').removeAttr('fill');
+//                 $('[stroke]').removeAttr('stroke');
+//                 $('[style]').removeAttr('style');
+//             },
+//             parserOptions: {xmlMode: true}
+//         }))
+//         .pipe(svgSprites({
+//             mode: {
+//                 svg: {
+//                     dimensionAttributes: true
+//                 },
+//                 symbol: {
+//                     sprite: './svg-symbols.svg',
+//                 }
+//             },
+
+//         }))
+//         .pipe(replace('<?xml version="1.0" encoding="utf-8"?>', ''))
+//         .pipe(dest('app/svg' + '/svgmin/'))
+//         .pipe(touch());
+// }
+
 function build() {
     return src([
         'app/css/style.min.css',
@@ -77,7 +111,7 @@ function watching() {
     watch(['app/*.html']).on('change', browserSync.reload);
 }
 
-
+//exports.svgSprites = svgSprites;
 exports.styles = styles;
 exports.watching = watching
 exports.browsersync = browsersync;
@@ -86,4 +120,4 @@ exports.images = images;
 exports.clearDist = clearDist
 
 exports.build = series(clearDist, images, build);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, scripts, browsersync, watching); //svgSprites
